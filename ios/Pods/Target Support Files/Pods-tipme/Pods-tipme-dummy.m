ccc69597161872e47db0ca3216971b729cb507b0
kinshukjain <kinshuk@sumologic.com>
2018-06-13 16:31:28 +0530

    adding cocoapods

---
+++ ios/Pods/Target Support Files/Pods-tipme/Pods-tipme-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_tipme : NSObject
+@end
+@implementation PodsDummy_Pods_tipme
+@end
---
+++ ios/Pods/Target Support Files/Pods-tipmeTests/Pods-tipmeTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_tipmeTests : NSObject
+@end
+@implementation PodsDummy_Pods_tipmeTests
+@end
